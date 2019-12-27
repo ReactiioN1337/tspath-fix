cbfab2583acafded23b26909f2d928fa196b256c
ReactiioN <reactiion@haze-productions.com>
2019-12-28 00:56:38 +0100

    ✨ Added getTSPathPackage()

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,10 @@
  * @copyright 2019, https://github.com/ReactiioN1337
  * @license   MIT
  */
-import { Log, LogLevel } from 'node-psr-log'
+import { Log, LogLevel }  from 'node-psr-log'
+import { parseDirectory } from './utils/directory'
 
 export const log = new Log(LogLevel.Everything, 'TSPath Fix', undefined, '')
+
+export const getTSPathPackage = () =>
+  require('./../package.json')
--- src/tspath.ts
+++ src/tspath.ts
@@ -4,13 +4,13 @@
  * @license   MIT
  */
 
-import { log } from './config'
+import { log, getTSPathPackage } from './config'
 
 class TSPath {
   private readonly _npm: any
 
   constructor() {
-    this._npm = require('./../package.json')
+    this._npm = getTSPathPackage()
     log.notice(`TSPath Fix version ${this._npm.version} has been loaded`)
   }
 }
