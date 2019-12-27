9961910f40c0efeb3581e945c9bc84e8398c339c
ReactiioN <reactiion@haze-productions.com>
2019-12-27 23:57:44 +0100

    🚀 Added exported log member

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+/**
+ * @author    ReactiioN
+ * @copyright 2019, https://github.com/ReactiioN1337
+ * @license   MIT
+ */
+import { Log, LogLevel } from 'node-psr-log'
+
+export const log = new Log(LogLevel.Everything, 'TSPath Fix', undefined, '')
