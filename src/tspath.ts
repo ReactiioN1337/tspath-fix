ca6d9a2b31e677cf2d856275decfbdac02cfcb47
ReactiioN <reactiion@haze-productions.com>
2019-12-28 01:55:21 +0100

    🚧 Updated business logic

--- src/tspath.ts
+++ src/tspath.ts
@@ -4,7 +4,11 @@
  * @license   MIT
  */
 
-import { log, getTSPathPackage } from './config'
+import { log, getTSPathPackage, getTSConfig } from './config'
+
+interface PathAliasInterface {
+  [key: string]: string
+}
 
 class TSPath {
   private readonly _npm: any
@@ -13,4 +17,27 @@ class TSPath {
     this._npm = getTSPathPackage()
     log.notice(`TSPath Fix version ${this._npm.version} has been loaded`)
   }
+
+  public async process() {
+    // todo: parse command line (--path)
+    const directory = process.cwd()
+
+    const aliases = await this.getAliases(directory)
+    if (aliases == undefined) {
+      return
+    }
+
+    console.log(aliases)
+  }
+
+  private async getAliases(directory: string): Promise<PathAliasInterface | undefined> {
+    const config = await getTSConfig(directory)
+    return config == undefined
+      ? undefined
+      : config.compilerOptions.paths
+  }
 }
+
+(new TSPath()).process()
+.then(() => {})
+.catch(err => log.critical(`${err}`))
